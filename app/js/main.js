a78e29c2526aaf2ec631a0578e94990da74bbc4d
Igor Farafonov <entertoinfo29@gmail.com>
2022-04-26 22:14:23 +0300

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function () {
-
+    $(".header__arrow-link").click(function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+	})
+	$(function () {
+		$('.map__popup-link').magnificPopup({
+			disableOn: 700,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+	
+			fixedContentPos: false
+		});
 })
